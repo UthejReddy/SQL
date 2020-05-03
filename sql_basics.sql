54052d97299e6699720994757bbdab4fb9fbf14b
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-05-03 14:01:10 +0530

    Update sql_basics.sql

--- sql_basics.sql
+++ sql_basics.sql
@@ -1,6 +1,7 @@
 -- show available databases
 show databases;
 -- create new database
+-- create comes under Data Definition Language
 create database company;
 use company;-- start using the database
 show tables;-- shows number of tables available in the database
@@ -8,12 +9,17 @@ show tables;-- shows number of tables available in the database
 create table employee(id integer, name varchar(10));
 desc employee;-- describe the table
 -- insert values into the table
+-- insert comes under Data Manupulating Language
 insert into employee values(1,'sham');
 insert into employee values (2,'pawan');
 insert into employee values (3,'om');
 
 select * from employee;-- use to show the table output
 set sql_safe_updates=0;-- this command enables to do changes in the table
+                                               
+-- delete comes under Data Manupulating Language                                               
 delete from employee where id = 3;-- delete the row with id 3
 select * from employee;
+                                               
+ -- update comes under DAta Manupulating Language                                              
 update employee set name ='jhonny' where id=2;-- allows the user to do changes in the table
